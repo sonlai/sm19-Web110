90f5a4b1e2f2271e93e9bb10ff45c756330619cd
sonlai <52689294+sonlai@users.noreply.github.com>
2019-08-12 17:44:44 -0700

    Add files via upload

---
+++ web110/contact.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<html lang="en">
+    
+ <head>
+  <title>SON LAI WEB110 Website</title>
+  <meta name="robot" content="noindex, nofollow" />
+  <meta name="viewpport" content="width=device-width" />
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="css/portal.css" />
+  <link rel="stylesheet" href ="css/nav.css" />
+     
+  <link rel="stylesheet" href="css/forms.css" />
+
+ </head>
+    
+    
+ <body>
+    <h1>SON LAI'S WEBSITE</h1>
+     
+    <header>
+        
+        <div class="topnav" id="myTopnav">
+            <a href="index.html" class="active">Welcome</a>
+            <a href="elements.html">HTML Elements</a>
+            <a href="AIA.html">AIA</a>
+            <a href="big/index.html">BIG</a>
+            <a href="http://w3schools.com" target="_blank">W3 School</a>
+            <a href="https://www.linkedin.com/learning/" target="_blank">LinkedIn Learning</a>
+            <a href="Flowchart.html">Flowchart</a>
+            <a href="fp/index.html">Final Project</a>
+            <a href="contact.php">Contact Son Lai</a>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+            </a>
+        </div>
+        
+     </header>
+     
+     <h2>Contact SON LAI</h2>
+<?php include "includes/simple.php";?>
+<p class="clear-recaptcha"></p>
+     
+     <div class="wrapper">
+     
+         <footer>
+                <p><small>&copy; 2019 by <a href="contact.php" target="_blank">SON LAI</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+         </footer>
+         
+    </div>
+ </body>
+    
+</html>
