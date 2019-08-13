515ae83e72dc4ff76b982a898570243e2707c87f
sonlai <52689294+sonlai@users.noreply.github.com>
2019-08-12 17:50:57 -0700

    Add files via upload

---
+++ web110/big/contact.php
@@ -0,0 +1,73 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<html lang="en">
+    
+ <head>
+  <title>SON LAI'S WEB DEVELOPER EXAMPLES AND RESOURCES</title>
+  <meta name="robot" content="noindex, nofollow" />
+  <meta name="viewpport" content="width=device-width" />
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="css/big.css" />
+  <link rel="stylesheet" href ="css/nav.css" />
+     <link rel="stylesheet" href ="css/forms.css" />
+     
+  <link href="path/to/lightbox.css" rel="stylesheet" />
+     
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+     
+    <script src="https://s3.amazonaws.com/menumaker/menumaker.min.js"></script>
+     
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
+     
+    <script src="js/script.js"></script>
+    <script src="path/to/lightbox.js"></script>
+     
+    
+    
+ </head>
+    
+ 
+    
+ <body>
+    <main>
+    <header>
+        <h1><a href="index.html">SON LAI'S WEB DEVELOPER EXAMPLES AND RESOURCES</a></h1>
+        <nav id="cssmenu">
+  <ul>
+     <li><a href="../index.html"><i class="fa fa-fw fa-institution"></i> WEB110</a></li>
+     <li><a href="index.html"><i class="fa fa-fw fa-home"></i> BIG Website</a></li>
+    
+    
+      <!--Drop down menu for research pages -->
+      <li><a href="#"><i class="fa fa-fw fa-bars"></i> Research Topics</a>
+        <ul>
+           <li><a href="accessibility.html"> Accessibility</a>
+           <li><a href="smo.html"> SMO</a></li>
+           <li><a href="forms.html"> HTML Forms</a></li>
+        </ul>
+     </li>
+      
+     <li><a href="gallery.html"><i class="fa fa-fw fa-camera-retro"></i> Image Gallery</a></li>
+      
+      <!--Drop down menu for Google -->
+     <li><a href="#"><i class="fa fa-fw fa-bars"></i> Google Tools</a>
+        <ul>
+           <li><a href="seo.html"><i class="fa fa-fw fa-desktop"></i> SEO</a></li>
+           <li><a href="calendar.html"><i class="fa fa-fw fa-calendar"></i> Calendar</a></li>
+           <li><a href="map.html"><i class="fa fa-fw fa-map-o"></i> Map</a></li>
+        </ul>
+     </li>
+     <li><a href="contact.php"><i class="fa fa-check-square" aria-hidden="true"></i> Contact Son Lai</a></li>
+  </ul>
+</nav>
+     </header>
+     
+    <h2>Contact SON LAI</h2>
+<?php include "includes/simple.php";?>
+<p class="clear-recaptcha"></p>
+     
+         <footer>
+                <p><small>&copy; 2019 by <a href="contact.php" target="_blank">SON LAI</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+         </footer>
+        </main>
+ </body>
+    </html>
